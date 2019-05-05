99a5810d42f65278e3f62bab5f33ae5175d1891a
CORP\twarren14 <twarren14@student.cscc.edu>
2019-05-04 20:54:03 -0400

    Making a change because forgot to fork the original code so i could make a pull request

--- src/main/java/edu/cscc/topics/quality/unit/DatabaseBackedMeaningOfLife.java
+++ src/main/java/edu/cscc/topics/quality/unit/DatabaseBackedMeaningOfLife.java
@@ -6,5 +6,6 @@ public class DatabaseBackedMeaningOfLife implements MeaningOfLifeMachine {
     @Override
     public String getMeaning() {
         throw new RuntimeException("Silly rabbit.  You don't hook up to a real database in a unit test setting.  That would be a bad idea!");
+        //literally nothing
     }
 }
