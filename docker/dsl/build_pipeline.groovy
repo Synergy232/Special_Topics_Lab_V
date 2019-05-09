288416e16049b9677bf3673afa5d59573079d424
Synergy232 <39544972+Synergy232@users.noreply.github.com>
2019-05-09 17:52:48 -0400

    Update build_pipeline.groovy

--- docker/dsl/build_pipeline.groovy
+++ docker/dsl/build_pipeline.groovy
@@ -1,7 +1,7 @@
 pipelineJob('build-pipeline-job') { 
 
   // This is the repo that contains the Jenkinsfile you want to use to define your pipeline
-  def repo = 'https://github.com/Synergy232/Special_Topics_Lab_V/tree/So_i_can_do_a_pull_request_because_i_messed_up_at_the_start'
+  def repo = 'https://github.com/Synergy232/Special_Topics_Lab_V'
 
   description("Your App Pipeline") 
   keepDependencies(false) 
@@ -18,7 +18,7 @@ pipelineJob('build-pipeline-job') {
       scm { 
         git { 
           remote { url(repo) } 
-          branches('master') 
+          branches('So_i_can_do_a_pull_request_because_i_messed_up_at_the_start') 
           scriptPath('Jenkinsfile') // The location of the Jenkinsfile that defines the pipeline job within the repo
           extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
         } 
